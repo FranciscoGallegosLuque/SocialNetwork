38a80bed2365e143b2169c4642e7a2540778b22a
francisco.gallegos.luque <franciscogallegosluque@gmail.com>
2025-02-19 19:34:26 -0300

    Initial Commit

---
+++ SocialNetwork/SocialNetworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialNetworkApp.swift
+//  SocialNetwork
+//
+//  Created by Francisco Manuel Gallegos Luque on 19/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct SocialNetworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SocialNetworkTests/SocialNetworkTests.swift
@@ -0,0 +1,17 @@
+//
+//  SocialNetworkTests.swift
+//  SocialNetworkTests
+//
+//  Created by Francisco Manuel Gallegos Luque on 19/02/2025.
+//
+
+import Testing
+@testable import SocialNetwork
+
+struct SocialNetworkTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
